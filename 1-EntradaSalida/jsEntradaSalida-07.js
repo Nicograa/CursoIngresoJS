c84382c2c2ec87d4796cd9461abc250df5532675
Nicoogra <nicoograa@gmail.com>
2024-08-13 10:44:24 -0300

    commit 3

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,71 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
-}
+	let numeroUno
+	let numeroDos
+	let resultado
+    
+    
+
+	numeroUno = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	numeroDos = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+    resultado = numeroUno + numeroDos;
+
+	alert("El resultado es "+ resultado);
+	
+ }
 
 function restar()
 {
+	let numeroUno
+	let numeroDos
+	let resultado
+    
+    
+
+	numeroUno = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	numeroDos = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+    resultado = numeroUno - numeroDos;
+
+	alert("El resultado es "+ resultado);
 	
 }
 
 function multiplicar()
 { 
-	
+	let numeroUno
+	let numeroDos
+	let resultado
+    
+    
+
+	numeroUno = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	numeroDos = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+    resultado = numeroUno * numeroDos;
+
+	alert("El resultado es "+ resultado);
 }
 
 function dividir()
 {
-	
+	let numeroUno
+	let numeroDos
+	let resultado
+    
+    
+
+	numeroUno = parseInt(document.getElementById('txtIdNumeroUno').value);
+
+	numeroDos = parseInt(document.getElementById('txtIdNumeroDos').value);
+
+    resultado = numeroUno / numeroDos;
+
+	alert("El resultado es "+ resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo
+	let aumento
+	let nuevoSueldo
+ 	
+ 	sueldo = parseFloat(document.getElementById('txtIdSueldo').value);
 
-	alert("ok");
+    aumento = sueldo * 10 /100;
+
+    nuevoSueldo = sueldo + aumento;
+
+    document.getElementById('txtIdResultado').value = nuevoSueldo;
+
+
+ 	 
 }

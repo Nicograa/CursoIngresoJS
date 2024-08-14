f2eb1779d09a123acc2ebff4327fcf4c36f87d2d
Nicoogra <nicoograa@gmail.com>
2024-08-14 14:39:10 -0300

    3

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,76 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+    
+    
+
+	precioUno = parseFloat(document.getElementById('txtIdPrecioUno').value);
+
+	precioDos = parseFloat(document.getElementById('txtIdPrecioDos').value);
+
+	precioTres = parseFloat(document.getElementById('txtIdPrecioTres').value);
+
+    suma = precioUno + precioDos + precioTres;
+
+	alert("El resultado es "+ suma);
 }
+
 function Promedio () 
 {
 	
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+	let promedio;
+    
+    
+
+	precioUno = parseFloat(document.getElementById('txtIdPrecioUno').value);
+
+	precioDos = parseFloat(document.getElementById('txtIdPrecioDos').value);
+
+	precioTres = parseFloat(document.getElementById('txtIdPrecioTres').value);
+
+    suma = precioUno + precioDos + precioTres;
+
+    promedio = suma / 3;
+
+    promedio = promedio.toFixed(2);
+
+	alert("El resultado es "+ promedio);
 }
+
+
 function PrecioFinal () 
 {
 	
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+	let iva;
+	let importeFinal;
+    
+    
+
+	precioUno = parseFloat(document.getElementById('txtIdPrecioUno').value);
+
+	precioDos = parseFloat(document.getElementById('txtIdPrecioDos').value);
+
+	precioTres = parseFloat(document.getElementById('txtIdPrecioTres').value);
+
+    suma = precioUno + precioDos + precioTres;
+
+    iva = suma * 21 /100;
+
+    importeFinal = suma + iva;
+
+    importeFinal = importeFinal.toFixed(2);
+
+	alert("El resultado es "+ importeFinal);
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,33 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
+	let temperaturaF;
+	let temperaturaC;
 	
+
+	temperaturaF = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+	temperaturaC = (temperaturaF - 32) * 5 / 9;
+
+	temperaturaC = temperaturaC.toFixed(2);
+
+	alert(temperaturaF +" grados Fahrenheit son " + temperaturaC +" grados centígrados");
+
+
 }
 
 function CentigradosFahrenheit () 
 {
 	
+	let temperaturaF;
+	let temperaturaC;
+	
+
+	temperaturaC = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+	temperaturaF = temperaturaC * 1.8 + 32;
+
+	temperaturaF = temperaturaF.toFixed(2);
+
+	alert(temperaturaC +" grados centígrados son " + temperaturaF +" grados Fahrenheit");
 }
